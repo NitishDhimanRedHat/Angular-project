da1a512edf0335b690266f608c0651a85f192ac1
NitishDhimanRedHat <nitishdhiman930@gmail.com>
2020-09-03 20:00:43 +0530

    created components and added routes in the project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TraningComponent } from './traning/traning.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: WelcomeComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'traning', component: TraningComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TraningComponent } from './traning/traning.component';
+import { CurrentTraningComponent } from './traning/current-traning/current-traning.component';
+import { NewTraningComponent } from './traning/new-traning/new-traning.component';
+import { PastTraningsComponent } from './traning/past-tranings/past-tranings.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TraningComponent,
+    CurrentTraningComponent,
+    NewTraningComponent,
+    PastTraningsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
